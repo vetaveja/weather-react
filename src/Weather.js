f410f578182eca9112f66ca69041f5921582c9b0
vetaveja <33002583+vetaveja@users.noreply.github.com>
2023-05-26 14:04:09 +0300

    transferred weather proj from codespace

---
+++ src/Weather.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let [query, setQuery] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=4f0e069tba9bdef6f3505aa3023cc7o5`;
+    axios.get(url).then(handleResponse);
+  }
+
+  function updateQuery(event) {
+    setQuery(event.target.value);
+  }
+
+  function handleResponse(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: response.data.condition.icon_url,
+      description: response.data.condition.description
+    });
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateQuery}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.condition}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
