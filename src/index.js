ad3db9b6525b3ce17d7b8fe3198002b5998e9512
vetaveja <33002583+vetaveja@users.noreply.github.com>
2023-06-02 12:38:49 +0300

    prepare proj with adding bootstrap and removing unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 
 import Weather from "./Weather";
@@ -9,10 +12,13 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <div className="container">
     <h1>Weather App</h1>
 
     <Weather />
     <br/>
+    <a className="btn btn-primary" href="/">Hello</a>
     <a href="https://github.com/vetaveja/weather-react">Open-sourced ptoject</a> made by Lisa Zelena.
+    </div>
   </StrictMode>
 );
